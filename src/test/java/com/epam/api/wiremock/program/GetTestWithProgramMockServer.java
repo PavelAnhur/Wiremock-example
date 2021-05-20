a4ef2946c0ecb1d8687b888eb7b83231ec968c4a
Pavel Anhur <pavelanhur@gmail.com>
2021-05-20 14:29:15 +0300

    Wiremock project

---
+++ src/test/java/com/epam/api/wiremock/program/GetTestWithProgramMockServer.java
@@ -0,0 +1,40 @@
+package com.epam.api.wiremock.program;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpStatus;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Optional;
+
+@Slf4j
+public class GetTestWithProgramMockServer extends GetQueryWithProgramMockConditions {
+
+    @Test
+    public void wireMockGETTestWithSecondEndPoint() {
+        SoftAssert softAssert = new SoftAssert();
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(URI.create(
+                String.format(getUrl(), getPort()) + getEndPoint()))
+                                      .GET()
+                                      .build();
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            int statusCode = response.statusCode();
+            log.info("Status code: {}", statusCode);
+            softAssert.assertEquals(statusCode, HttpStatus.SC_OK, "Status code is not correct!");
+            Optional<String> header = response.headers().firstValue("Content-Type");
+            log.info("Response headers: {}", header.toString());
+            softAssert.assertEquals(header, Optional.of("applications/json"),
+                    "Content type is not correct!");
+        } catch (IOException | InterruptedException e) {
+            log.error(e.getMessage());
+        }
+        softAssert.assertAll();
+    }
+}
